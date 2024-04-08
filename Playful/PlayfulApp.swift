86aba9d476b9ac00948ec86f3d5d8860d03cb25a
pfriedrix <pfriedrix@icloud.com>
2024-04-08 18:33:51 +0300

    Initial Commit

---
+++ Playful/PlayfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayfulApp.swift
+//  Playful
+//
+//  Created by Pfriedrix on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
